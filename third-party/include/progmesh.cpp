f86cc27bb764b5b1d69ad13e04b10162474014ea
toxieainc <toxie@ainc.de>
2026-03-31 12:35:55 +0200

    fix mingw warnings

--- third-party/include/progmesh.cpp
+++ third-party/include/progmesh.cpp
@@ -12,8 +12,12 @@
 #include <cstdio>
 #include <cmath>
 #include <cstdlib>
+#ifndef WIN32_LEAN_AND_MEAN
 #define WIN32_LEAN_AND_MEAN
+#endif
+#ifndef NOMINMAX
 #define NOMINMAX
+#endif
 #include <windows.h>
 #include <cassert>
 
